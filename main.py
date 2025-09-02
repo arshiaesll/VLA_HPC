ede674d69efb887eb463de687313c03f32957872
arshiaesll <arshia.esl83@gmail.com>
2025-09-02 10:07:41 -0400

    more code

--- main.py
+++ main.py
@@ -17,7 +17,27 @@ def run_model():
         trust_remote_code=True
     )
 
-    model = AutoTokenizer.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    pipe = pipeline(
+        "text-generation",
+        model = model,
+        tokenizer=tokenizer
+    )
+
+    messages = [
+        {"role": "system", "content": "You are a helpful AI assistant."},
+        {"role": "user", "content": "Can you provide ways to eat combinations of bananas and dragonfruits?"},
+        {"role": "assistant", "content": "Sure! Here are some ways to eat bananas and dragonfruits together: 1. Banana and dragonfruit smoothie: Blend bananas and dragonfruits together with some milk and honey. 2. Banana and dragonfruit salad: Mix sliced bananas and dragonfruits together with some lemon juice and honey."},
+        {"role": "user", "content": "What about solving an 2x + 3 = 7 equation?"},
+    ]    
+
+    generation_args = {
+        "max_new_tokens": 500,
+        "return_full_text": False,
+        "do_sample": False,
+        "use_cache": False
+    }
 
 
 if __name__ == "__main__":
